2a43150db381be22054c9c28c4dedae0a07e93ca
Brian Chapman <chapman.brian86@gmail.com>
2023-02-09 16:12:11 -0500

    Reworked the form and set up emailjs to receive messages from it. General restyling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,94 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { validateEmail } from "../utils/helpers";
 
 export default function Contact() {
-  const [name, setName] = useState("");
+  const form = useRef();
+  const [contactName, setContactName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleInputChange = (e) => {
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    if (inputType === "name") {
-      setName(inputValue);
-    } else if (inputType === "email") {
-      setEmail(inputValue);
-    } else {
-      setMessage(inputValue);
+  const [sent, setSent] = useState(false);
+ 
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await emailjs.sendForm(
+        "service_xp3108e",
+        "template_aax8x2b",
+        form.current,
+        "4bQ_rOUKTctgQNHs-"
+      );
+      console.log(result.text);
+    } catch (error) {
+      console.log(error.text);
     }
+    setSent(true);
   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (!validateEmail(email) || !name) {
-      setErrorMessage(
-        "Please provide a valid name and email address. Thank you! :)"
-      );
-      return;
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "contactName") {
+      setContactName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else {
+      setMessage(value);
     }
   };
 
   return (
-    <section className="container form-container">
-      <h1 className="text-center">Say Hey!</h1>
-      <form>
-        <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">
-            Name
-          </label>
-          <input
-            value={name}
-            type="text"
-            name="name"
-            className="form-control"
-            onChange={handleInputChange}
-            id="exampleFormControlInput1"
-            placeholder="Your Name"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">
-            Email address
-          </label>
-          <input
-            value={email}
-            type="email"
-            name="email"
-            className="form-control"
-            onChange={handleInputChange}
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label for="exampleFormControlTextarea1" className="form-label">
-            Your Message
-          </label>
-          <textarea
-            value={message}
-            name="message"
-            className="form-control"
-            onChange={handleInputChange}
-            id="exampleFormControlTextarea1"
-            rows="3"
-          />
-        </div>
-        <div className="col-12">
-          <button type="submit" onClick={handleFormSubmit} className="btn" id="form-btn">
-            Send Message
-          </button>
-        </div>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
+    <section className="container form-container shadow-lg">
+      {sent ? (
+        <h1 className="text-center mt-3 email-header">Thanks for dropping a line!</h1>
+      ) : (
+        <>
+          <h1 className="text-center mt-3 email-header">Say Hey!</h1>
+          <form ref={form} onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">
+                Name
+              </label>
+              <input
+                value={contactName}
+                type="text"
+                name="contactName"
+                className="form-control"
+                onChange={handleInputChange}
+                id="exampleFormControlInput1"
+                placeholder="Your Name"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">
+                Email address
+              </label>
+              <input
+                value={email}
+                type="email"
+                name="email"
+                className="form-control"
+                onChange={handleInputChange}
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label for="exampleFormControlTextarea1" className="form-label">
+                Your Message
+              </label>
+              <textarea
+                value={message}
+                name="message"
+                className="form-control"
+                placeholder="Your message here..."
+                onChange={handleInputChange}
+                id="exampleFormControlTextarea1"
+                rows="3"
+              />
+            </div>
+            <div className="col-12">
+              <button type="submit" className="btn" id="form-btn">
+                Send Message
+              </button>
+            </div>
+          </form>
+        </>
       )}
+      <div>
+        <p className="error-text"></p>
+      </div>
     </section>
   );
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -19,7 +19,7 @@ export default function Portfolio() {
        <ProjectCard title="Pet Play Date" img={ petPlayDate } Alt="Pet Play Date App Screenshot" description ="Collaboration on a full-stack app. Create a profile for your pet, match profiles with other pet owners in your area, and schedule pet play dates!" githubLink ="https://github.com/BrianSChapman/pet-playdate"
               deployment="https://salty-sierra-16506.herokuapp.com/"  />
        <ProjectCard title="My Friendly Monster" img={ monster } Alt="My Friendly Monster App Screenshot" description ="A collaborative project utilizing a MERN stack (With GraphQL & Apollo for querying and database communication). Adopt a monster as a pet and interact with it online." githubLink ="https://github.com/BrianSChapman/my-friendly-monster"
-              deployment="https://vast-inlet-40115.herokuapp.com/" />
+              deployment="https://radiant-garden-89222.herokuapp.com/" />
        <ProjectCard title="Word of My Life" img={ wordOfMyLife } Alt="Word of My Life App Screenshot" description ="Collaborative effort. Log on to the app to receive a word of the day along with any relevant media related to it (Movie, books, video games). Making learning a new word fun!" githubLink ="https://github.com/BrianSChapman/word-of-my-life"
                deployment="https://brianschapman.github.io/word-of-my-life/" />
         </div>
