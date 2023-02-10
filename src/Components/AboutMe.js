ebcef5b70d5e91df16fa1cecc5309d34f29e15a8
Brian Chapman <chapman.brian86@gmail.com>
2023-02-10 14:43:00 -0500

    Finishing up About me page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,36 +1,39 @@
 import React from "react";
 import headshot from "../Images/Headshot.jpg";
 
+
 export default function AboutMe() {
   return (
-    
-    <section id="about" className="container">
+
+    <section id="about" className>
+      <h1 id="welcome" className="text-center mb-5">Welcome (Afio mai)!</h1>
+
       <div className="row align-items-center"></div>
       <div className="col-9">
-
-      <div className="card mb-3 border-dark mb-3" id="bio-card">
         <div className="row">
           <div className="col-md-4">
             <img
-              className="img-fluid rounded-start border-dark mb-3"
+              className="rounded-start border-dark mb-3"
               id="bio-photo"
               src={headshot}
               alt="Brian Chapman Headshot"
             ></img>
           </div>
           <div className="col-md-8">
-            <h2 className="card-title mt-3">About Me</h2>
-            <p className="card-text">
-              Hello! My name is Brian, a Southern California hailing from Philadelphia. Energetic and self-sufficient problem solver striving to utilize
+            <p className="about-me-text">
+              Hey friends! I'm Brian Chapman, a Southern California native hailing from the mean streets of Philadelphia. </p>
+
+              <p className="about-me-text" id="cover-letter-text"> <em>"Energetic and self-sufficient problem solver striving to utilize
               soft skills learned through customer service and facilities
               coordinating paired with newly acquired web development skills to
-              transition into a front-end web development position.
+              transition into a front-end web development position."</em>
             </p>
-            <aside>
-            <h4>Quick Facts:</h4>
+            <p className="about-me-text">...Kinda standard cover letter stuff, right? Real talk - I love working with people, problem solving is my jam, and I'm passionate about creating positive experiences (for the end user and for my colleagues). </p>
+            <aside className="text-center">
+            <h3 id="quick-facts">Quick Facts:</h3>
             <ul>
               <li>Samoan/ Pacific Islander</li>
-              <li>Musician</li>
+              <li>Musician (Toured with a punk band)</li>
               <li>Unapologetic Tiki Enthusiast</li>
               <li>Forever an optimist</li>
             </ul>
@@ -38,7 +41,7 @@ export default function AboutMe() {
           </div>
         </div>
       </div>
-      </div>
+
     </section>
   );
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,29 +1,53 @@
 import React from "react";
-import petPlayDate from "../Images/petPlayDate.png"
+import petPlayDate from "../Images/petPlayDate.png";
 import monster from "../Images/monster-dashboard-snippet.png";
-import wordOfMyLife from "../Images/wordOfMyLife.png"
-import textEditor from "../Images/textEditor.png"
-import githubIcon from "../Images/githubIcon.png"
-import herokuLogo from "../Images/herokulogo.png"
+import wordOfMyLife from "../Images/wordOfMyLife.png";
+import textEditor from "../Images/textEditor.png";
+import githubIcon from "../Images/githubIcon.png";
+import herokuLogo from "../Images/herokulogo.png";
 import ProjectCard from "../Components/ProjectCard";
-
-
-
 
 export default function Portfolio() {
   return (
     <section id="portfolio" className="container">
-        <h1 className="text-center">Projects</h1>
-        <p className="text-center mt-2 pb-2">(Click the image for deployment or the icon to check out the codebase)</p>
+      <h1 className="text-center">Projects</h1>
+      <p className="text-center mt-2 pb-2">
+        (Click the image for deployment or the icon to check out the codebase)
+      </p>
       <div className="row">
-       <ProjectCard title="Pet Play Date" img={ petPlayDate } Alt="Pet Play Date App Screenshot" description ="Collaboration on a full-stack app. Create a profile for your pet, match profiles with other pet owners in your area, and schedule pet play dates!" githubLink ="https://github.com/BrianSChapman/pet-playdate"
-              deployment="https://salty-sierra-16506.herokuapp.com/"  />
-       <ProjectCard title="My Friendly Monster" img={ monster } Alt="My Friendly Monster App Screenshot" description ="A collaborative project utilizing a MERN stack (With GraphQL & Apollo for querying and database communication). Adopt a monster as a pet and interact with it online." githubLink ="https://github.com/BrianSChapman/my-friendly-monster"
-              deployment="https://radiant-garden-89222.herokuapp.com/" />
-       <ProjectCard title="Word of My Life" img={ wordOfMyLife } Alt="Word of My Life App Screenshot" description ="Collaborative effort. Log on to the app to receive a word of the day along with any relevant media related to it (Movie, books, video games). Making learning a new word fun!" githubLink ="https://github.com/BrianSChapman/word-of-my-life"
-               deployment="https://brianschapman.github.io/word-of-my-life/" />
-        </div>
-
+        <ProjectCard
+          title="Pet Play Date"
+          img={petPlayDate}
+          Alt="Pet Play Date App Screenshot"
+          description="Collaboration on a full-stack app. Create a profile for your pet, match profiles with other pet owners in your area, and schedule pet play dates!"
+          githubLink="https://github.com/BrianSChapman/pet-playdate"
+          deployment="https://salty-sierra-16506.herokuapp.com/"
+        />
+        <ProjectCard
+          title="My Friendly Monster"
+          img={monster}
+          Alt="My Friendly Monster App Screenshot"
+          description="A collaborative project utilizing a MERN stack (With GraphQL & Apollo for querying and database communication). Adopt a monster as a pet and interact with it online."
+          githubLink="https://github.com/BrianSChapman/my-friendly-monster"
+          deployment="https://radiant-garden-89222.herokuapp.com/"
+        />
+        <ProjectCard
+          title="Word of My Life"
+          img={wordOfMyLife}
+          Alt="Word of My Life App Screenshot"
+          description="Collaborative effort. Log on to the app to receive a word of the day along with any relevant media related to it (Movie, books, video games). Making learning a new word fun!"
+          githubLink="https://github.com/BrianSChapman/word-of-my-life"
+          deployment="https://brianschapman.github.io/word-of-my-life/"
+        />
+        <ProjectCard
+          title="Not Another Text Editor"
+          img={textEditor}
+          Alt="Text Editor Screenshot"
+          description="Progressive web app text editor that can be installed onto your device, giving the user functionality even when offline."
+          githubLink="https://github.com/BrianSChapman/textual-healing"
+          deployment="https://limitless-oasis-11120.herokuapp.com/"
+        />
+      </div>
     </section>
   );
 }
